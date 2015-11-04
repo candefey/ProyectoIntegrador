5761ea2d5fe5803fe623995ccbb7beccf1a30be5
candefey <candelariafey@gmail.com>
2015-11-04 13:51:27 -0300

    Faltaban Entities Ejercicio y TipoRutina

---
+++ Entidades/Ejercicio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ejercicio
+    {
+        private int idEjercicio;
+        private string nombre;
+        private string descripcion;
+        private double tiempo;
+
+        public int IdEjercicio { get; set; }
+        public string Nombre{ get; set; }
+        public string Descripcion { get; set; }
+        public double Tiempo { get; set; }
+    }
+}
---
+++ Entidades/TipoRutina.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class TipoRutina
+    {
+        private int idTipoRutina ;
+        private string nombre;
+        private string descripcion;
+
+        public int IdTipoRutina { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
